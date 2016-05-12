d9b9423574861c1c57218c7f1cb7138da86c3bb8
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2016-05-12 15:45:21 +0300

    make request to external rate service, return the result

--- js/config.js
+++ js/config.js
@@ -1,2 +1,4 @@
 const config = new Map();
-config.set('baseUrl', 'http://api.fixer.io/latest')
+config.set('baseUrl', 'http://api.fixer.io/latest');
+config.set('baseUrlError', 'Currency data cannot be retrieved at the moment. Please check the server.');
+module.exports = config;
--- js/server/app.js
+++ js/server/app.js
@@ -1,11 +1,27 @@
 'use strict'
-
 let express = require('express');
 let app = express();
+let request = require('request');
+let config  = require('../config.js');
 
-app.get('/', (req, res) => {
-  console.log(req);
-})
+app.get('/rates', (req, res) => {
+  // Promise
+  request({method: 'GET', uri: config.get('baseUrl')}, (error, response, body) => {
+    if(response.statusCode == '200'){
+      res.send(body);
+    } else {
+      res.send(config.get('baseUrlError'))
+    }
+  });
+});
+
+let getCurrencyRates = () => {
+  let responseBody = request({method: 'GET', uri: config.get('baseUrl')}, (error, response, body) => {
+    return body;
+  });
+
+  return responseBody;
+}
 
 app.listen(5555, () => {
   console.log('listening...');
