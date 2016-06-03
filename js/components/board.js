55e1dfe0bfae4b9898c0c7006d6e7eb9c9390dbf
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2016-06-03 10:14:15 +0300

    add posibility to change the base currency

--- js/components/board.js
+++ js/components/board.js
@@ -6,10 +6,10 @@ import Rate from './rate.js'
 class Board extends React.Component {
   constructor(){
     super();
-    this.state = { rates: '', error: false, base: '' }
+    this.state = { rates: '', error: false, base: 'EUR' }
   }
 
-  componentDidMount(){
+  getRates(base = ''){
     let promise = new Promise((resolve, reject) => {
       let request = new XMLHttpRequest();
 
@@ -20,7 +20,7 @@ class Board extends React.Component {
           reject(request.statusText);
         }
       }
-      request.open('GET', config.get('serverUrl'));
+      request.open('GET', `${config.get('serverUrl')}?base=${base}`);
       request.send();
 
     }).then((response) => {
@@ -29,13 +29,26 @@ class Board extends React.Component {
     }, (response) => {
       this.setState({ error: true })
     });
+  }
 
+  changeBaseValue(e){
+    console.log(e.target.value);
+    this.getRates(e.target.value);
+  }
+
+  componentDidMount(){
+    this.getRates(this.state.base);
   }
 
   render() {
     let rates = this.state.rates;
+    let base = this.state.base
     return (
       <div>
+        <select onChange={ this.changeBaseValue.bind(this) } defaultValue={base}>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+        </select>
         <div>Base: { this.state.base}</div>
         <div>Rates: { Object.keys(rates).map((rate, i) => {
           return (
--- js/server/app.js
+++ js/server/app.js
@@ -7,7 +7,8 @@ let config  = require('../config.js');
 app.get('/rates', (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  request({method: 'GET', uri: config.get('baseUrl')}, (error, response, body) => {
+  let base = req.query['base'];
+  request({method: 'GET', uri: `${config.get('baseUrl')}?base=${base}`}, (error, response, body) => {
     if(response.statusCode == '200'){
       res.send(body);
     } else {
